392a0849dbec2a596bf464ae9ab2cc1e4ebd40f1
刘广 <15779280545@163.com>
2022-04-23 14:23:13 +0800

    添加一个文件装换

--- src/main/java/com/lg/project/notes/common/utils/MultipartFileUtils.java
+++ src/main/java/com/lg/project/notes/common/utils/MultipartFileUtils.java
@@ -3,6 +3,20 @@ package com.lg.project.notes.common.utils;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.FormBodyPart;
+import org.apache.http.entity.mime.FormBodyPartBuilder;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.ContentBody;
+import org.apache.http.entity.mime.content.InputStreamBody;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
@@ -10,6 +24,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName MultipartFileUtils
@@ -52,5 +69,52 @@ public class MultipartFileUtils {
         }
         return item;
     }
+
+    public static String uploadFile(String url , List<MultipartFile> fileList, String fileParamName, Map<String,String>headerParams, Map<String,String>otherParams) {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        String result = "";
+        if(fileList== null || fileList.size() <=0){
+            return result;
+        }
+        try {
+
+            HttpPost httpPost = new HttpPost(url);
+            //添加header
+            for (Map.Entry<String, String> e : headerParams.entrySet()) {
+                httpPost.addHeader(e.getKey(), e.getValue());
+            }
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+
+            builder.setCharset(Charset.forName("utf-8"));
+            builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);//加上此行代码解决返回中文乱码问题
+            for(MultipartFile file :fileList){
+                String fileName = file.getOriginalFilename();
+                builder.addBinaryBody("file", file.getInputStream(), ContentType.MULTIPART_FORM_DATA, fileName);
+            }
+            for (Map.Entry<String, String> e : otherParams.entrySet()) {
+                builder.addTextBody(e.getKey(), e.getValue());// 类似浏览器表单提交，对应input的name和value
+            }
+            HttpEntity entity = builder.build();
+            httpPost.setEntity(entity);
+            HttpResponse response = httpClient.execute(httpPost);// 执行提交
+            HttpEntity responseEntity = response.getEntity();
+            if (responseEntity != null) {
+                // 将响应内容转换为字符串
+                result = EntityUtils.toString(responseEntity, Charset.forName("UTF-8"));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
 
 }
